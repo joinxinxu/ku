5d6e8b1d8eefe5a99357773e81f9a888e7ae4343
xinxu.wang <xinxu.wang@oqiji.com>
2019-08-22 18:28:33 +0800

    Initial Commit

---
+++ gitKu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitKu
+//
+//  Created by wangxinxu on 2019/8/22.
+//  Copyright © 2019 wangxinxu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitKu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitKu
+//
+//  Created by wangxinxu on 2019/8/22.
+//  Copyright © 2019 wangxinxu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
